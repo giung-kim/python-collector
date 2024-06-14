62edd25c71d19e336caf671af2eb195b0ed48ffe
giung-kim <gw9977@gmail.com>
2024-06-14 15:03:31 +0900

    Proj setup

--- app/main.py
+++ app/main.py
@@ -3,14 +3,21 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 
-BASE_DIR=Path(__file__).resolve().parent    #현재파일의 경로 + Parent(현재파일의 부모폴더)
+BASE_DIR = (
+    Path(__file__).resolve().parent
+)  # 현재파일의 경로 + Parent(현재파일의 부모폴더)
 
 app = FastAPI()
-templates = Jinja2Templates(directory=BASE_DIR/"templates")
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse(
-        request=request, name="item.html", context={"id": id,"data":"hello fast api"}
-    )
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    context = {"request": request, "title": "Collector"}
+    return templates.TemplateResponse(name="./index.html", context=context)
+
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    context = {"request": request, "title": "Collector", "keyword": q}
+    return templates.TemplateResponse(name="./index.html", context=context)
