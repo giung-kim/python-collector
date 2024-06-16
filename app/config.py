ab4490be4d73599cf937d19871443efa07f3d420
giung-kim <gw9977@gmail.com>
2024-06-16 16:23:01 +0900

    Add book search scraper

--- app/config.py
+++ app/config.py
@@ -5,12 +5,19 @@ from typing import Optional
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 경로 출력 및 파일 존재 여부 확인
+json_path = BASE_DIR / "secrets.json"
+print(f"BASE_DIR: {BASE_DIR}")
+print(f"JSON Path: {json_path}")
+print(f"JSON File Exists: {json_path.exists()}")
+
 
 def get_secret(
     key: str,
     default_value: Optional[str] = None,
     json_path: str = str(BASE_DIR / "secrets.json"),
 ):
+
     with open(json_path) as f:
         secrets = json.loads(f.read())
     try:
--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
+from app.models import mongodb
+from app.models.book import BookModel
+
 
 BASE_DIR = (
     Path(__file__).resolve().parent
@@ -13,6 +16,10 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    book = BookModel(
+        keyword="파이썬", publisher="BJPublic", price=1200, image="me.png"
+    )  # Test
+    await mongodb.engine.save(book)  # DB에 저장
     context = {"request": request, "title": "Collector"}
     return templates.TemplateResponse(name="./index.html", context=context)
 
@@ -21,3 +28,16 @@ async def root(request: Request):
 async def search(request: Request, q: str):
     context = {"request": request, "title": "Collector", "keyword": q}
     return templates.TemplateResponse(name="./index.html", context=context)
+
+
+@app.on_event("startup")
+def on_app_start():
+    """before app starts"""
+    mongodb.connect()
+    print("DB Connected")
+
+
+@app.on_event("shutdown")
+def on_app_shutdown():
+    """after app shutdown"""
+    mongodb.close()
